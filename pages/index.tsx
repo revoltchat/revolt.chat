ada5f2c0d1d60df83d484e6a0ebe198ec0b9db59
Paul Makles <paulmakles@gmail.com>
2020-06-29 12:29:05 +0100

    Add form to landing page.

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,20 @@ import classNames from 'classnames';
 import styles from './index.module.scss';
 import flex from '../components/util/flex.module.scss';
 
+import { useState, FormEvent } from 'react';
+import axios from 'axios';
+
 export default function Home() {
+    let [ sent, setSent ] = useState(false);
+    let [ email, setEmail ] = useState('');
+
+    function submit(e: FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+        setSent(true);
+
+        axios.post('/api/send', { email });
+    }
+
     return (
         <div>
             <Head>
@@ -22,6 +35,17 @@ export default function Home() {
                             <div className={styles.primary}>
                                 <h2>Don't bother with chat apps that don't respect your privacy.</h2>
                                 <h3>Communication is critical.<br/>Privacy is essential.</h3>
+                                {
+                                    sent ? <h4>Thanks! We'll keep you up to date.</h4> :
+                                    <form className={styles.signup} onSubmit={submit}>
+                                        <input
+                                            placeholder="Enter enter your email."
+                                            value={email}
+                                            onChange={e => setEmail(e.currentTarget.value)}
+                                        />
+                                        <button>Join waiting list.</button>
+                                    </form>
+                                }
                             </div>
                         </div>
                         <div className={classNames(flex.column, styles.heroImage)}>
