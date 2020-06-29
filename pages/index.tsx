537d6a21b65e6051affdcb41a6db99555b8cf7b3
Paul Makles <paulmakles@gmail.com>
2020-06-29 12:50:55 +0100

    Add open graph.

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,6 +23,16 @@ export default function Home() {
             <Head>
                 <title>Revolt</title>
                 <link rel="icon" href="/favicon.ico" />
+                <meta name="viewport" content="width=device-width, initial-scale=0.75, user-scalable=0"></meta>
+                <meta property="og:title" content="REVOLT" />
+                <meta property="og:description" content="Don't bother with chat apps that don't respect your privacy. REVOLT is a brand new chat platform designed around you." />
+                <meta property="og:url" content="https://revolt.chat" />
+                <meta property="og:image" content="/embed.png" />
+                <meta property="twitter:card" content="summary_large_image"></meta>
+                <meta property="twitter:image" content="/embed.png"></meta>
+                <meta property="og:image:type" content="image/png" />
+                <meta property="og:image:width" content="1280" />
+                <meta property="og:image:height" content="720" />
             </Head>
         
             <main>
