1a470c147c51f3ab666b804a6a733d89a1882b13
Martin Löffler <git@fatalerrorcoded.eu>
2020-06-30 13:26:50 +0200

    Move Signup Form over to its own file

---
+++ components/SignupForm.tsx
@@ -0,0 +1,57 @@
+import styles from './SignupForm.module.scss';
+
+import { useState, FormEvent } from 'react';
+import axios from 'axios';
+
+enum Status {
+    NotSent = 1,
+    Sending,
+    Sent,
+    ServerError,
+    InvalidEmail,
+}
+
+export default function SignupForm() {
+    let [ status, setStatus ] = useState(Status.NotSent);
+    let [ email, setEmail ] = useState('');
+
+    function submit(e: FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+        if (email === '') {
+            setStatus(Status.InvalidEmail);
+            return;
+        }
+
+        setStatus(Status.Sending);
+
+        axios.post('/api/send', { email }).then((response) => {
+            if (response.status === 200) {
+                setStatus(Status.Sent);
+            } else if (response.status === 400) setStatus(Status.InvalidEmail);
+            else setStatus(Status.ServerError);
+        }).catch(() => setStatus(Status.ServerError));
+    }
+
+    if (status === Status.Sending || status === Status.Sent) {
+        if (status === Status.Sending) return <div className={styles.form}><h4>Loading</h4></div>;
+        else return (
+            <div className={styles.form}>
+                <h4>Thanks! We'll keep you up to date.</h4>
+            </div>
+        );
+    } else return (
+        <div className={styles.form}>
+            <form className={styles.signup} onSubmit={submit}>
+                <input
+                    type="email"
+                    placeholder="Please enter your email."
+                    value={email}
+                    onChange={e => setEmail(e.currentTarget.value)}
+                />
+                <button>Join waiting list.</button>
+            </form>
+            { status === Status.ServerError && <h4>An error has occured, please try again later.</h4> }
+            { status === Status.InvalidEmail && <h4>Please provide a valid email</h4> }
+        </div>
+    );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,21 +3,9 @@ import classNames from 'classnames';
 
 import styles from './index.module.scss';
 import flex from '../components/util/flex.module.scss';
-
-import { useState, FormEvent } from 'react';
-import axios from 'axios';
+import SignupForm from '../components/SignupForm';
 
 export default function Home() {
-    let [ sent, setSent ] = useState(false);
-    let [ email, setEmail ] = useState('');
-
-    function submit(e: FormEvent<HTMLFormElement>) {
-        e.preventDefault();
-        setSent(true);
-
-        axios.post('/api/send', { email });
-    }
-
     return (
         <div>
             <Head>
@@ -45,18 +33,7 @@ export default function Home() {
                             <div className={styles.primary}>
                                 <h2>Don't bother with chat apps that don't respect your privacy.</h2>
                                 <h3>Communication is critical.<br/>Privacy is essential.</h3>
-                                {
-                                    sent ? <h4>Thanks! We'll keep you up to date.</h4> :
-                                    <form className={styles.signup} onSubmit={submit}>
-                                        <input
-                                            type="email"
-                                            placeholder="Please enter your email."
-                                            value={email}
-                                            onChange={e => setEmail(e.currentTarget.value)}
-                                        />
-                                        <button>Join waiting list.</button>
-                                    </form>
-                                }
+                                <SignupForm />
                             </div>
                         </div>
                         <div className={classNames(flex.column, styles.heroImage)}>
