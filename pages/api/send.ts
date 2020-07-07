c2957226c03f1dd5ff507ef2623e47a86bf5c8cf
Martin Löffler <git@fatalerrorcoded.eu>
2020-07-07 13:59:09 +0200

    Database for waiting list

--- pages/api/send.ts
+++ pages/api/send.ts
@@ -1,11 +1,19 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import nc from 'next-connect';
+
 import axios from 'axios';
 import shortid from 'shortid';
+import { ulid } from 'ulid';
+
+import databaseMiddleware from '../../components/middleware/database';
 
 const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-    if (req.method === 'POST') {
+const handler = nc<NextApiRequest, NextApiResponse>();
+handler.use(databaseMiddleware);
+
+handler.post(async (req: NextApiRequest, res: NextApiResponse) => {
+    try {
         if (req.body.email === undefined) {
             res.status(400).json({ success: false, error: "No email provided" });
             return;
@@ -16,24 +24,34 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
             return;
         }
 
-        let referrer = '';
-        if (typeof req.body.referrer === 'string' && req.body.referrer.length >= 3 && req.body.referrer.length <= 30)
-            referrer = req.body.referrer;
-
-        let referral = shortid.generate();
-
-        try {
-            await axios.post(process.env.WEBHOOK_URL, {
-                "content": req.body.email + ":" + referral + ":" + referrer
-            });
+        const coll = (req as any).db.collection('users');
+        let result = await coll.findOne({ email: req.body.email });
+        
+        if (result !== null) {
+            res.status(200).json({ success: true, referral: result.referral, verified: result.verified });
+        } else {
+            let referrer = null;
+            if (typeof req.body.referrer === 'string')
+                referrer = await coll.findOne({ referral: req.body.referrer });
             
-            res.status(200).json({ success: true, referral });
-        } catch (err) {
-            console.error(err);
-            res.status(500).json({ success: false, error: "Internal server error" });
+            let referral = shortid.generate();
+            await coll.insertOne({
+                _id: ulid(),
+                email: req.body.email,
+                referral, verified: true,
+                referrer_id: referrer !== null ? referrer._id : null
+            });
+
+            axios.post(process.env.WEBHOOK_URL, {
+                "content": req.body.email + ":" + referral
+            }).then(() => {}).catch((err) => console.warn(err));
+
+            res.status(200).json({ success: true, referral: referral, verified: true });
         }
-    } else {
-        res.setHeader("Allow", "POST");
-        res.status(405).send("");
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ success: false, error: "Internal server error" });
     }
-}
+});
+
+export default handler;
