eb3ff701b808cd6e28d8895f669e9701596f22de
Paul Makles <me@insrt.uk>
2024-06-05 21:53:04 +0100

    fix: the final hack for comments box....

--- components/posts/CommentsView.tsx
+++ components/posts/CommentsView.tsx
@@ -1,5 +1,5 @@
 import Script from "next/script";
-import { useEffect } from "react";
+import { useEffect, useLayoutEffect } from "react";
 import styled from "styled-components";
 
 const GiscusView = styled.div`
@@ -7,37 +7,27 @@ const GiscusView = styled.div`
 `;
 
 const CommentsView = () => {
-    useEffect(() => {
-        const HolyMotherOfHack = setInterval(() => {
-            const el = document.querySelector(".giscus");
-            if (el) {
-                clearInterval(HolyMotherOfHack);
-                document.getElementById("giscusTarget")!.appendChild(el);
-            }
-        });
+    useLayoutEffect(() => {
+        const script = document.createElement("script");
+        script.src = "https://giscus.app/client.js";
+        script.setAttribute("data-repo", "revoltchat/revolt");
+        script.setAttribute("data-repo-id", "MDEwOlJlcG9zaXRvcnkzOTM0MDIwNTY=");
+        script.setAttribute("data-category", "Announcements");
+        script.setAttribute("data-category-id", "DIC_kwDOF3LWyM4B-k-m");
+        script.setAttribute("data-mapping", "title");
+        script.setAttribute("data-strict", "0");
+        script.setAttribute("data-reactions-enabled", "1");
+        script.setAttribute("data-emit-metadata", "0");
+        script.setAttribute("data-input-position", "bottom");
+        script.setAttribute("data-theme", "https://revolt.chat/giscus.css");
+        script.setAttribute("data-lang", "en");
+        script.setAttribute("data-loading", "lazy");
+        script.crossOrigin = "anonymous";
+        script.async = true;
+        document.getElementById("giscusTarget").appendChild(script);
     }, []);
 
-    return (
-        <GiscusView id="giscusTarget">
-            <Script
-                src="https://giscus.app/client.js"
-                data-repo="revoltchat/revolt"
-                data-repo-id="MDEwOlJlcG9zaXRvcnkzOTM0MDIwNTY="
-                data-category="Announcements"
-                data-category-id="DIC_kwDOF3LWyM4B-k-m"
-                data-mapping="title"
-                data-strict="0"
-                data-reactions-enabled="1"
-                data-emit-metadata="0"
-                data-input-position="bottom"
-                data-theme="https://revolt.chat/giscus.css"
-                data-lang="en"
-                data-loading="lazy"
-                crossOrigin="anonymous"
-                async
-            />
-        </GiscusView>
-    );
+    return <GiscusView id="giscusTarget" />;
 };
 
 export default CommentsView;
