e9746c4ddc6d79ca0a3a83d0104abdecc8e8954a
Paul Makles <me@insrt.uk>
2025-02-06 14:09:14 +0000

    chore: switch to legacy google play build for now

--- src/lib/consts.ts
+++ src/lib/consts.ts
@@ -2,7 +2,7 @@ export default class Consts {
     static readonly MSSTORE_URL =
         "ms-windows-store://pdp/?ProductId=9nmtvmrj7n1k" as const
     static readonly PLAYSTORE_URL =
-        "https://play.google.com/store/apps/details?id=chat.revolt" as const
+        "https://play.google.com/store/apps/details?id=chat.revolt.app.twa" as const
     static readonly FLATHUB_URL =
         "https://flathub.org/apps/chat.revolt.RevoltDesktop" as const
     static readonly TESTFLIGHT_URL =
