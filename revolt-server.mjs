7222c94d01366fe0be2c1563b21e1528c71027f1
jen <herself@starlightjen.com>
2025-03-15 20:38:15 +0100

    fix: 404 page not appearing properly

--- revolt-server.mjs
+++ revolt-server.mjs
@@ -1,6 +1,7 @@
 import express from "express"
 import { handler as ssrHandler } from "./dist/server/entry.mjs"
 import "dotenv/config"
+import process from "node:process"
 
 const app = express()
 
@@ -9,6 +10,10 @@ app.set("x-powered-by", false)
 const base = "/"
 app.use(base, express.static("dist/client/"))
 app.use(ssrHandler)
+app.use((req, res, next) => {
+    req.url = "/404"
+    ssrHandler(req, res, next)
+})
 
 const port = Number(process.env.PORT) || 8080
 
